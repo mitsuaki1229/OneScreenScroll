17fa58a29ac570b5d1cbb5b15e50cd3813782ce8
mitsuaki1229 <mitsuaki1229@gmail.com>
2020-12-22 22:26:29 +0900

    Create project.

---
+++ Shared/OneScreenScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneScreenScrollApp.swift
+//  Shared
+//
+//  Created by Mitsuaki Ihara on 2020/12/22.
+//
+
+import SwiftUI
+
+@main
+struct OneScreenScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
